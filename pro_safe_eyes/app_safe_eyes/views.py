b70b202e4e547d155759d7db20a10db7b6bbd0f4
unknown <w0rkadm1n@encnot010.tmh-gw.jp>
2025-02-20 12:56:19 +0900

    0220 1256

--- pro_safe_eyes/app_safe_eyes/views.py
+++ pro_safe_eyes/app_safe_eyes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic       import ListView
 
 # Create your views here.
+
+# ホーム画面
+class HomeView(ListView):
+     template_name = "home.html"
+    #  model = Comment
+     
+     # クラスベースビューの呼び出し時、クエリセットをログインユーザーのもののみに絞って返す。
+    #  def get_queryset(self):
+    #        current_user = self.request.user
+    #        return Comment.objects.filter(user= current_user) 
