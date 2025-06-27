f352060caf9adbde0043c19460aa8861c822a12b
T-watanabe9 <byzer927@gmail.com>
2025-06-27 20:15:47 +0900

    0627 2015

--- ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
+++ ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Card
+from django.forms import Textarea
+from django.db import models
 
 # Register your models here.
 class CardAdmin(admin.ModelAdmin):
      list_display = ('id' , 'name' , 'category' ,  'cost' , 'color' , 'race')
      search_fields = ('name', 'category' , 'effect_text', 'race', 'explain' )
      list_filter = ('category', 'cost' , 'color')
+
+     # TextField の見た目を改善
+     formfield_overrides = {
+          models.TextField: {
+              'widget': Textarea(attrs={'rows': 5, 'cols': 60})
+               },
+     }
+
 
 admin.site.register(Card , CardAdmin)
