4b498fefa8cdf7ae5d8413db478e7cde109a9b5d
T-watanabe9 <byzer927@gmail.com>
2025-06-27 22:43:58 +0900

    0627 2243

--- ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
+++ ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
@@ -1,11 +1,16 @@
+from django.db import models
 from django.contrib import admin
+from django.forms import Textarea
 from .models import Card
 from adminsortable2.admin import SortableAdminMixin
 
 # Register your models here.
 class CardAdmin(SortableAdminMixin, admin.ModelAdmin):
-     list_display = ('id' , 'name' , 'category' ,  'cost' , 'color' , 'race')
+     list_display = ('id' , 'name' , 'category' ,  'cost' , 'color' , 'race' , 'prioriy')
      search_fields = ('name', 'category' , 'effect_text', 'race', 'explain' )
      list_filter = ('category', 'cost' , 'color')
+     formfield_overrides = {
+         models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 60})},
+     }
 
 admin.site.register(Card , CardAdmin)
