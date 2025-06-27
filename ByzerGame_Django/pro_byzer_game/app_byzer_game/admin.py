b0478e33315d4b6c40ccb879ebdbccd70c077e01
T-watanabe9 <byzer927@gmail.com>
2025-06-27 23:10:25 +0900

    0627 2310

--- ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
+++ ByzerGame_Django/pro_byzer_game/app_byzer_game/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.db import models
 from django.contrib import admin
 from django.forms import Textarea
@@ -5,14 +6,24 @@ from .models import Card
 from adminsortable2.admin import SortableAdminMixin
 
 # Register your models here.
+
+# カスタムフォームの定義
+class CardForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = '__all__'
+        widgets = {
+            'effect_text': Textarea(attrs={
+                'style': 'min-height: 200px; max-height: none !important; ',
+                'rows': 20,  # rowsはstyleではなく属性として指定
+            }),
+        }
+
 class CardAdmin(SortableAdminMixin, admin.ModelAdmin):
+     form = CardForm
      list_display = ('id' , 'name' , 'category' ,  'cost' , 'color' , 'race' , 'priority')
      search_fields = ('name', 'category' , 'effect_text', 'race', 'explain' )
      list_filter = ('category', 'cost' , 'color')
-     formfield_overrides = {
-         models.TextField: {'widget': Textarea(attrs={
-    'style': 'min-height: 200px; max-height: none !important; width: 90%;',
-})},
-     }
+     
 
 admin.site.register(Card , CardAdmin)
